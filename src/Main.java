b3aeae99c7006e60e4cd7ada4d739fc4e83fe5fb
MitasMax90 <Rostishka90@mail.ru>
2021-12-24 16:24:36 +0300

    b1111

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Plane plane1 = new Plane();
-        plane1.flyStraight(50);
-        plane1.flyUp(80,90);
-        plane1.turnRight();
-        plane1.flyStraight(30);
-        plane1.dropBombs();
-        plane1.flyStraight(40);
-        plane1.flyDown(100,10);
-        plane1.fillFuel();
-        plane1.flyDown(100,10);
-        plane1.fillFuel();
-        plane1.fillFuel();
+        System.out.println("brunch");
     }
 }
 
